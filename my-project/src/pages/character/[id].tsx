fb0bebced4928f1c8082c5b9590e29ab44c1633a
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-11-24 16:37:22 -0300

    update project

--- my-project/src/pages/character/[id].tsx
+++ my-project/src/pages/character/[id].tsx
@@ -8,6 +8,7 @@ interface CharacterData {
   name: string;
   status: string;
   species: string;
+  gender: string;
   origin: {
     name: string;
   };
@@ -18,7 +19,9 @@ interface CharacterData {
 }
 
 function PrevCard() {
-  const [characterData, setCharacterData] = useState<CharacterData | null>(null);
+  const [characterData, setCharacterData] = useState<CharacterData | null>(
+    null
+  );
   const router = useRouter();
 
   useEffect(() => {
@@ -33,13 +36,15 @@ function PrevCard() {
         }
 
         // Faz a requisição à API com base no id da URL
-        const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character/${id}`
+        );
 
         // Verifica se a resposta foi bem-sucedida
         if (response.ok) {
           // Converte a resposta para JSON
           const data: CharacterData = await response.json();
-          
+
           // Atualiza o estado do componente com os dados recebidos
           setCharacterData(data);
         } else {
@@ -59,15 +64,27 @@ function PrevCard() {
       <Header />
       <div className="container mx-auto w-full h-screen">
         {characterData && (
-          <div className="flex flex-row items-center justify-center gap-5 h-screen w-full">
-            <img className="rounded-full" src={characterData.image} alt={characterData.name} />
-            <div className="flex flex-col">
-              <h1 className="text-2xl font-bold">{characterData.name}</h1>
-              <p>{`${characterData.status} - ${characterData.species}`}</p>
-              <h2 className="text-xl mt-3 text-gray-700">First seen in:</h2>
-              <p>{characterData.origin.name}</p>
-              <h2 className="text-xl mt-3 text-gray-700">Last known location:</h2>
-              <p>{characterData.location.name}</p>
+          <div className="flex flex-col items-center justify-center h-screen w-full">
+            <div className="flex flex-col gap-1">
+              <h1 className="text-3xl font-bold text-center">
+                {characterData.name}
+              </h1>
+              <img src={characterData.image} alt={characterData.name} />
+              <p>
+                <strong>Status:</strong> {`${characterData.status}`}
+              </p>
+              <p>
+                <strong>Gender:</strong> {`${characterData.gender}`}
+              </p>
+              <p>
+                <strong>Species:</strong> {`${characterData.species}`}
+              </p>
+              <p>
+                <strong>Origin:</strong> {characterData.origin.name}
+              </p>
+              <p>
+                <strong>Location:</strong> {characterData.location.name}
+              </p>
             </div>
           </div>
         )}
